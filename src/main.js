30d2e9da93624dc4c80e88beb938598c20602bed
beckersjonas <beckersjonas@gmail.com>
2025-01-23 13:03:03 +0100

    Lots of changes...

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
-const displayFontSizes = () => {
-  const ro = new ResizeObserver((entries) => {
-    entries.forEach(entry => {
-      const fs = (parseFloat(entry.target.computedStyleMap().get("font-size")) / 16).toFixed(2, "0")
-      entry.target.querySelector("span").innerHTML = `-> ${fs}`
-    })
-  })
+// const displayFontSizes = () => {
+//   const ro = new ResizeObserver((entries) => {
+//     entries.forEach(entry => {
+//       const fs = (parseFloat(entry.target.computedStyleMap().get("font-size")) / 16).toFixed(2, "0")
+//       entry.target.querySelector("span").innerHTML = `-> ${fs}`
+//     })
+//   })
   
-  const divs = document.querySelectorAll("div[class*='fs-']")
-  divs.forEach(div => ro.observe(div))
-}
-displayFontSizes()
+//   const divs = document.querySelectorAll("div[class*='fs-']")
+//   divs.forEach(div => ro.observe(div))
+// }
+// displayFontSizes()
+
+// function* generatorFunc() {
+//   yield 1
+//   console.log("after 1")
+//   yield 2
+//   console.log("after 2")
+// }
+
+// const g1 = generatorFunc()
+// console.log(g1.next())
+// console.log(g1.next())
+// console.log(g1.next())
+
+
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
+import pluginPurgeCss from "vite-plugin-purgecss-updated-v5"
 
 export default defineConfig({
   root: resolve(__dirname, "src/"),
@@ -9,7 +10,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         home: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/about.html"),
       },
     },
   },
+  plugins: [pluginPurgeCss()],
 })
